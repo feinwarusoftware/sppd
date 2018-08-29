da886f27f5eae7025f8f0b7ab462addce7fa2811
Dragon1320 <lukasznie1320@gmail.com>
2018-08-30 00:50:56 +0100

    fixed 99% of shit

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,25 @@
 const express = require("express");
 const router = express.Router();
 
+const cards = require("../static/cards.json");
+
 router.get("/", (req, res) => {
 
     return res.render("index");
+});
+
+router.get("/:card", (req, res) => {
+
+    const card = cards.find(e => {
+        return e.Image === req.params.card;
+    });
+
+    if (card === undefined) {
+
+        return res.render("404");
+    }
+
+    return res.render("card", { card });
 });
 
 module.exports = router;
