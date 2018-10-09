a9656ecc248e586862cfa9585ae13f4a5285032f
Matthew Watt <MattheousDT@users.noreply.github.com>
2018-10-09 20:32:04 +0100

    fuck me like how father maxi fucks butters

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const router = express.Router();
 
-const cards = require("../static/cards.json");
+const cards = require("../static/js/card-stats.json");
 
 router.get("/", (req, res) => {
 
