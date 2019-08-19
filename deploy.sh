7be8c021e05b82f175bb41f7a796d4d1376f73ba
Lukasz Niezabitowski <lukasznie1320@gmail.com>
2019-08-19 21:15:19 +0100

    cm?

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)"
+chmod 600 .travis/travis
+ssh-add .travis/travis
+
+git config --global push.default matching
+git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git push deploy develop
+
+ssh git@$IP -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  # Any command go here:
+  sh prod.sh
+EOF
+
