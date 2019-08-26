035824ab14b735b53016f72bbcf8adba463976c2
Dragon1320 <lukasznie1320@gmail.com>
2019-08-26 15:48:13 +0100

    yeeted temp.sh

--- temp.sh
+++
@@ -1,4 +0,0 @@
-( cd ui && npx webpack --config webpack.dev.js )
-( cd src && token=rawrxd port=80 node . )
-
-# ( cd src && token=fuck123redlynxGei! port=80 mongo_user=bot mongo_pass=fein123waruGei! mongo_source=35.243.239.197 node . )
